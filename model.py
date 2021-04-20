0c546f743af6afebaca09148ece0e82d43391b00
Jaša Pozne <jasa.pozen@student.fmf.uni-lj.si>
2021-04-20 17:27:50 +0200

    Dodane funkcije za izračun sledi, transponiranje matrike in fun. za množenje dveh matrik (le-to je potrebno še dopolniti).

--- model.py
+++ model.py
@@ -30,12 +30,12 @@ class Matrika:
             raise Exception("POZOR! Matriki sta raličnih velikosti!")
         #če pogoji seštevanja so "mogoči"
         else:
-            m = self.vrstice
-            n = self.stolpci
+            n = self.vrstice
+            m = self.stolpci
             vsota = []
             vrstica = []
-            for i in range(m):
-                for j in range(n):
+            for i in range(n):
+                for j in range(m):
                     vrstica.append(self.matrika[i][j] + other.matrika[i][j])
                 vsota.append(vrstica)
             return Matrika(vsota)
@@ -50,12 +50,73 @@ class Matrika:
             raise Exception("POZOR! Matriki sta raličnih velikosti!")
         #pogoji so mogoči
         else:
-            m = self.vrstice
-            n = self.stolpci
+            n = self.vrstice
+            m = self.stolpci
             razlika = []
             vrstica = []
-            for i in range(m):
-                for j in range(n):
+            for i in range(n):
+                for j in range(m):
                     vrstica.append(self.matrika[i][j] - other.matrika[i][j])
                 razlika.append(vrstica)
             return Matrika(razlika)
+    def sled_matrike(self):
+        #funkcija, ki izračuna sled matrike
+        if not self.vrstice == self.stolpci:
+            raise Exception("POZOR! Matrika ni kvadratna!")
+        else:
+            n = self.vrstice
+            sled = 0
+            for i in range(n):
+                sled += self.matrika[i][i]
+            return sled
+
+    def transponiraj_matriko(self):
+    # fukcija, ki bo transponirala matriko
+        n = self.vrstica
+        m = self.stolpec
+        transponiranka = []
+        vrstica = []
+        for i in range(n):
+            for j in range(m):
+                vrstica.append(self.matrika[j][i])
+                transponiranka.append(vrstica)
+        return transponiranka
+
+    def potencirajmo_celene_matrike(self,k): 
+    #funkcija, ki bo potencirala matriko
+    #pogoji so, da je kvadratna torej
+        if self.vrstice != self.stolpci:
+            return Exception("POZOR! Matrika ni kvadratna!")
+
+        else:
+            n = self.vrstice 
+            m = self.stolpci
+            matrikca = []
+            vrstica = []
+            for i in range(n):
+                for j in range(m):
+                    vrstica.append(self.matrika[i**int(k)][j**int(k)])
+                    matrikca.append(vrstica)
+            return matrikca
+
+    def __mul__(self,other):
+        #funkcija, za množenje matrike 
+            if self.stolpci != other.vrstice:
+                return Exception("POZOR! Dimenzije matrik se ne ujemajo!")
+            else:
+                n = self.vrstice
+                m = other.stolpci
+                l = other.vrstice
+                matrikca = []
+                #POTREBNO BO ŠE DEFINIRATI MATRIKO, Z DIM. m×l LE-TA PA BO IMELA POVSOD SAME NIČLE
+                for i in range(n):
+                    for j in range(m):
+                        for k in range(l):
+                            matrikca.append(self.matrika[i][k]*other.matrika[k][j])
+                return matrikca
+
+
+# funkcija, ki izračuna inverz matrike
+#funkcija, za množenje s vektorji
+#funkcija za MNK
+# sistem linearnih enačb 
